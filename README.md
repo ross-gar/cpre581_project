21c4db51f18eb9e46db0870c89a6fabfd3a6c015
Ross Garrison <ross.garrison13@gmail.com>
2016-10-30 00:33:27 +0000

    End of day commit.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cpre581_project
+This project tested on linux kernel version 4.4.21-v7+
+
+Make to generate pmu_module.ko kernel module
+
+Use taskset to force kernel to load kernel module on core 0 of quad core processor:
+sudo taskset -c 0 insmod pmu_module.ko
+
+Also use taskset to force test programs to use core 0 so that the PMU registers are enabled:
+sudo taskset -c0 ./testProg
--- main.cpp
+++ main.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 
 /*
-Coprocessor 15
-opcode1 0
-destination is x,
-c9 and c13 are coprocessor registers
-opcode2 0
+MCR: Move to coprocessor from ARM register
+MRC: Move to ARM register from coprocessor
+
+Instruction format:
+MCR/MRC coproc, #op1, src/dest, CRn, CRm, #op2
+CRn and CRm are coprocessor registers
 */
-unsigned int access_value (void)
-{
-	unsigned int x = 0;
-	asm("MCR p15,  #0, %0, c9, c13, #0" : "=r" (x));
-	return x;
-}
 
 int main ()
 {
+	//asm("MCR p15, #0, %0, C9, C12, #1 \n\t" : : "r"(1));
 	unsigned int x = 0;
-	x = access_value();
-	std::cout<<"Returned value: "<< x<<std::endl;
-	return 0;  
+	asm("MRC p15,  #0, %0, c9, c12, #0 \n\t" : "=r" (x)); // Performance monitor control register
+	//asm("MRC p15,  #0, %0, c9, c13, #0 \n\t" : "=r" (x));
+	std::cout<<std::hex<<"Returned value: 0x"<< x<<std::endl;
+	return 0;
 }
