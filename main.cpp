22d6cf239810898ddce5d4f8d0ba304899c1ebbe
Ross Garrison <ross.garrison13@gmail.com>
2016-11-03 00:45:43 +0000

    End of day commit

--- main.cpp
+++ main.cpp
@@ -11,10 +11,29 @@ CRn and CRm are coprocessor registers
 
 int main ()
 {
-	//asm("MCR p15, #0, %0, C9, C12, #1 \n\t" : : "r"(1));
+
 	unsigned int x = 0;
-	asm("MRC p15,  #0, %0, c9, c12, #0 \n\t" : "=r" (x)); // Performance monitor control register
-	//asm("MRC p15,  #0, %0, c9, c13, #0 \n\t" : "=r" (x));
-	std::cout<<std::hex<<"Returned value: 0x"<< x<<std::endl;
+	// Enable all counters by writing a 1 to the LSB of performance monitor control register
+	asm volatile("MCR p15, #0, %0, C9, C12, #0 \n\t" : : "r"(0x41072001));
+	// Write to count enable set register
+	asm volatile("MCR p15, #0, %0, C9, C12, #1 \n\t" : : "r"(0x8000000f));
+
+	// Write to event type selection register
+	asm volatile("MCR p15, #0, %0, C9, C13, #1 \n\t" : : "r"(0x00000007));
+	asm volatile("MRC p15,  #0, %0, c9, c13, #1 \n\t" : "=r" (x)); 
+	std::cout<<std::hex<<"Event type 1: 0x"<< x<<std::endl;
+	// Write to select counter
+	asm volatile("MCR p15, #0, %0, C9, C12, #5 \n\t" : : "r"(0x00000001));
+	asm volatile("MRC p15,  #0, %0, c9, c13, #1 \n\t" : "=r" (x)); 
+	std::cout<<std::hex<<"Event type 2: 0x"<< x<<std::endl;
+
+	/*	
+	asm volatile("MRC p15,  #0, %0, c9, c12, #0 \n\t" : "=r" (x)); // Performance monitor control register
+	std::cout<<std::hex<<"PMCR Check: 0x"<< x<<std::endl;
+	asm volatile("MRC p15,  #0, %0, c9, c13, #0 \n\t" : "=r" (x));  //Cycle count register
+	std::cout<<std::hex<<"Cycle Count Register: 0x"<< x<<std::endl;
+	asm volatile("MRC p15,  #0, %0, c9, c13, #2 \n\t" : "=r" (x)); 
+	std::cout<<std::hex<<"Mystery Value: 0x"<< x<<std::endl;
+	*/
 	return 0;
 }
