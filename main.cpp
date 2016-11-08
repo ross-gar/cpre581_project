47ca87754c9b2228165d6444c77e7f102d635e1f
Ross Garrison <ross.garrison13@gmail.com>
2016-11-08 02:51:28 +0000

    End of day commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 /*
 MCR: Move to coprocessor from ARM register
 MRC: Move to ARM register from coprocessor
@@ -9,14 +8,53 @@ MCR/MRC coproc, #op1, src/dest, CRn, CRm, #op2
 CRn and CRm are coprocessor registers
 */
 
-int main ()
-{
+// Define event codes for the ARM A7 architecture
+#define MEM_READ			0x06
+#define MEM_WRITE			0x07
+#define IMM_BRANCH		0x0D
+#define BRANCH_MISPRED	0x10
+#define L2_ACCESS			0x16
+#define L2_REFILL			0x17
 
-	unsigned int x = 0;
+void init_counters ()
+{
 	// Enable all counters by writing a 1 to the LSB of performance monitor control register
 	asm volatile("MCR p15, #0, %0, C9, C12, #0 \n\t" : : "r"(0x41072001));
 	// Write to count enable set register
 	asm volatile("MCR p15, #0, %0, C9, C12, #1 \n\t" : : "r"(0x8000000f));
+}
+
+void reset_counters ()
+{
+	// Reset all counters by writing a 1 to bit 1 of performance monitor control register
+	asm volatile("MCR p15, #0, %0, C9, C12, #0 \n\t" : : "r"(0x41072003));
+	// Clear reset bit
+	asm volatile("MCR p15, #0, %0, C9, C12, #0 \n\t" : : "r"(0x41072001));
+}
+
+unsigned int read_counter(unsigned int idx)
+{
+	unsigned int result;
+	// Write to select counter
+	asm volatile("MCR p15, #0, %0, C9, C12, #5 \n\t" : : "r"(idx));
+	// Read counter
+	asm volatile("MRC p15,  #0, %0, c9, c13, #2 \n\t" : "=r"(result));
+	return result; 
+}
+
+void set_event(unsigned int idx, unsigned int event)
+{
+	// Write to select counter
+	asm volatile("MCR p15, #0, %0, C9, C12, #5 \n\t" : : "r"(idx));
+	// Write desired event
+	asm volatile("MCR p15, #0, %0, C9, C13, #1 \n\t" : : "r"(event));
+}
+
+int main ()
+{
+
+	unsigned int x = 0;
+	init_counters();
 
 	// Write to event type selection register
 	asm volatile("MCR p15, #0, %0, C9, C13, #1 \n\t" : : "r"(0x00000007));
