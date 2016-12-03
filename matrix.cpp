82fcc50dd1305b4ac3af335ac5a1b067e3054eff
Ross Garrison <ross.garrison13@gmail.com>
2016-12-03 21:56:31 +0000

    Refine matrix and simple loop with automatic 25 run averaging

--- matrix.cpp
+++ matrix.cpp
@@ -3,19 +3,25 @@
 
 int main ()
 {
+
 	// Initialize variables
 	unsigned int l2_refill = 0, l2_access = 0, cycle_count = 0, temp = 0;
-	unsigned int block_size = 4;
+	unsigned int l2_refill_sum = 0, l2_access_sum = 0, cycle_count_sum = 0;
+	unsigned int block_size = 2;
+	unsigned int num_runs = 25;
 	unsigned int matrix_dim = 128;
 	unsigned int matrix_a [matrix_dim][matrix_dim];
 	unsigned int matrix_b [matrix_dim][matrix_dim];
 	unsigned int matrix_c [matrix_dim][matrix_dim];
 
+	std::cout<<"All tests run "<<num_runs<<" times and results averaged"<<std::endl;
+	std::cout<<"Block size: "<<block_size<<std::endl;
+
 	// Intialize matrices with some data
-	for (int i = 0;  i <= matrix_dim; i++) {
-		for (int j = 0; j <= matrix_dim; j++) {
-			matrix_a[i][j] = i + 3 + j*2;
-			matrix_b[i][j] = i + 7 + j*3;
+	for (int i = 0;  i < matrix_dim; i++) {
+		for (int j = 0; j < matrix_dim; j++) {
+			matrix_a [i][j] = i + 3 + j*2;
+			matrix_b [i][j] = i + 7 + j*3;
 			matrix_c [i][j] = 0;
 		}
 	} 
@@ -27,45 +33,87 @@ int main ()
 	set_event(1, CYCLE_COUNTER);
 	set_event(2, L2_ACCESS);
 
-	// Normal matrix multiplication
-	for (int i = 0;  i <= matrix_dim; i++) {
-		for (int j = 0; j <= matrix_dim; j++) {
-			temp = 0;
-			for (int k = 0; k <= matrix_dim; k++) {
-				temp = temp + matrix_a [i][k] * matrix_b [k][j];			
-			}
-			matrix_c [i][j] = temp;
-		}	
-	}
+	/* 
+	-----------------------------------------------------------------------------------
+	Normal matrix multiplication test	
+	-----------------------------------------------------------------------------------
+	*/
 
-	// Read counter result
+	// Perform 10 test runs
+	for (int run = 0; run <num_runs; run ++) {
+		reset_counters();
+		// Normal matrix multiplication
+		for (int i = 0;  i < matrix_dim; i++) {
+			for (int j = 0; j < matrix_dim; j++) {
+				temp = 0;
+				for (int k = 0; k < matrix_dim; k++) {
+					temp = temp + matrix_a [i][k] * matrix_b [k][j];			
+				}
+				matrix_c [i][j] = temp;
+			}	
+		}
+
+	// Read counter results
 	l2_refill = read_counter(0);
 	cycle_count = read_counter(1);
 	l2_access = read_counter(2);
-	std::cout<<std::dec<<"Standard L2 refill: "<<l2_refill<<"\tL2 access: "<<l2_access<<"\tCycle count: "<<cycle_count<<std::endl;
-	reset_counters();
+	// Accumulate counter results
+	l2_refill_sum += l2_refill;
+	cycle_count_sum += cycle_count;
+	l2_access_sum += l2_access;
+	} 
 
-	// Blocking matrix multiplication
-	for (int jj = 0; jj <= matrix_dim; jj = jj + block_size) {
-		for (int kk = 0; kk <= matrix_dim; kk = kk + block_size) {
-			for (int i = 0; i <= matrix_dim; i++) {
-				for (int j = jj; j <= std::min(jj+block_size, matrix_dim); j++) {
-					temp = 0;
-					for (int k = kk; k <= std::min(kk+block_size, matrix_dim); k++) {
-						temp = temp + matrix_a [i][k] * matrix_b [k][j];
-					}
-					matrix_c [i][j] = matrix_c [i][j] + temp;
-				}			
+	// Average run counts
+	l2_refill_sum = l2_refill_sum / num_runs;
+	cycle_count_sum = cycle_count_sum / num_runs;
+	l2_access_sum = l2_access_sum / num_runs;
+	// Output results
+	std::cout<<std::dec<<"Standard L2 refills: "<<l2_refill_sum<<"\tL2 accesses: "<<l2_access_sum<<"\tCycle count: "<<cycle_count_sum<<std::endl;
+	// Clear results
+	l2_refill_sum = 0;
+	cycle_count_sum = 0;
+	l2_access_sum = 0;
+
+	/* 
+	-----------------------------------------------------------------------------------
+	Block matrix multiplication test	
+	-----------------------------------------------------------------------------------
+	*/
+
+	// Perform 10 test runs
+	for (int run = 0; run <num_runs; run ++) {
+		reset_counters();
+		// Block matrix multiplication
+		for (int jj = 0; jj < matrix_dim; jj = jj + block_size) {
+			for (int kk = 0; kk < matrix_dim; kk = kk + block_size) {
+				for (int i = 0; i < matrix_dim; i++) {
+					for (int j = jj; j < std::min(jj+block_size, matrix_dim); j++) {
+						temp = 0;
+						for (int k = kk; k < std::min(kk+block_size, matrix_dim); k++) {
+							temp = temp + matrix_a [i][k] * matrix_b [k][j];
+						}
+						matrix_c [i][j] = matrix_c [i][j] + temp;
+					}			
+				}
 			}
 		}
-	}
 
-	// Read counter result
+	// Read counter results
 	l2_refill = read_counter(0);
 	cycle_count = read_counter(1);
 	l2_access = read_counter(2);
-	std::cout<<std::dec<<"Block    L2 refill: "<<l2_refill<<" \tL2 access: "<<l2_access<<"\tCycle count: "<<cycle_count<<std::endl;
-	reset_counters();
+	// Accumulate counter results
+	l2_refill_sum += l2_refill;
+	cycle_count_sum += cycle_count;
+	l2_access_sum += l2_access;
+	}
+
+	// Average run counts
+	l2_refill_sum = l2_refill_sum / num_runs;
+	cycle_count_sum = cycle_count_sum / num_runs;
+	l2_access_sum = l2_access_sum / num_runs;
+	// Output results
+	std::cout<<std::dec<<"Block    L2 refills: "<<l2_refill_sum<<" \tL2 accesses: "<<l2_access_sum<<"\tCycle count: "<<cycle_count_sum<<std::endl;
 
 	return 0;
 }
--- simple_loop.cpp
+++ simple_loop.cpp
@@ -5,49 +5,110 @@ int main ()
 {
 	// Initialize variables
 	unsigned int branch_miss = 0, cycle_count = 0;
+	unsigned int branch_miss_sum = 0, cycle_count_sum = 0;
 	unsigned int array_length = 100;
+	unsigned int num_runs = 25;
 	unsigned int data_array [array_length];
+
+	std::cout<<"All tests run "<<num_runs<<" times and results averaged"<<std::endl;
 
 	// Setup counters
 	init_counters();
-	reset_counters();
 	set_event(0, BRANCH_MISPRED);
 	set_event(1, CYCLE_COUNTER);
 
-	// Perform work
-	// Intialize array
-	for (int i = 0;  i <=array_length; i++) {
-		data_array[i] = i + 3;
-	} 
+	/* 
+	-----------------------------------------------------------------------------------
+	Array initialization test
+	-----------------------------------------------------------------------------------
+	*/
 
-	branch_miss = read_counter(0);
-	cycle_count = read_counter(1);
-	std::cout<<"Array init       branch misses: "<<branch_miss<<"\tcycle count: "<<cycle_count<<std::endl;
-	reset_counters();
-
-	// Perform some arbitrary operation on the odd elements of the array
-	// Simple way
-	for (int i = 0;  i <=array_length; i++) {
-		// Check for odd	
-		if (i % 2 == 1) {
+	// Perform 10 test runs
+	for (int run = 0; run <num_runs; run ++) {
+		reset_counters();
+		// Write array
+		for (int i = 0;  i < array_length; i++) {
 			data_array[i] = i + 3;
-		}
-	} 
-
+		} 
+	
+	// Read counter results
 	branch_miss = read_counter(0);
 	cycle_count = read_counter(1);
-	std::cout<<"Simple operation branch misses: "<<branch_miss<<"\tcycle count: "<<cycle_count<<std::endl;
-	reset_counters();
-
-	// Smart way
-	for (int i = 1;  i <=array_length; i = i + 2) {
-		data_array[i] = i + 3;
+	// Accumulate counter results
+	branch_miss_sum += branch_miss;
+	cycle_count_sum += cycle_count;
 	}
 
+	// Average run counts
+	branch_miss_sum = branch_miss_sum/num_runs;
+	cycle_count_sum = cycle_count_sum/num_runs;
+	// Output results
+	std::cout<<"Array init       branch misses: "<<branch_miss_sum<<"\tcycle count: "<<cycle_count_sum<<std::endl;
+	// Clear results
+	branch_miss_sum = 0;
+	cycle_count_sum = 0;
+
+	/* 
+	-----------------------------------------------------------------------------------
+	Simple odd element loop test
+	-----------------------------------------------------------------------------------
+	*/
+
+	// Perform 10 test runs
+	for (int run = 0; run <num_runs; run ++) {
+		reset_counters();
+		// Simple way
+		for (int i = 0;  i < array_length; i++) {
+			// Check for odd	
+			if (i % 2 == 1) {
+				data_array[i] = i + 3;
+			}
+		} 
+
+	// Read counter results
 	branch_miss = read_counter(0);
 	cycle_count = read_counter(1);
-	std::cout<<std::dec<<"Smart operation  branch misses: "<<branch_miss<<"\tcycle count: "<<cycle_count<<std::endl;
-	reset_counters();
+	// Accumulate counter results
+	branch_miss_sum += branch_miss;
+	cycle_count_sum += cycle_count;
+	}
+
+	// Average run counts
+	branch_miss_sum = branch_miss_sum/num_runs;
+	cycle_count_sum = cycle_count_sum/num_runs;
+	// Output results
+	std::cout<<"Simple operation branch misses: "<<branch_miss_sum<<"\tcycle count: "<<cycle_count_sum<<std::endl;
+	// Clear results
+	branch_miss_sum = 0;
+	cycle_count_sum = 0;
+
+	/* 
+	-----------------------------------------------------------------------------------
+	Smart odd element loop test
+	-----------------------------------------------------------------------------------
+	*/
+
+	// Perform 10 test runs
+	for (int run = 0; run <num_runs; run ++) {
+		reset_counters();
+		// Smart way
+		for (int i = 1;  i < array_length; i = i + 2) {
+			data_array[i] = i + 3;
+		}
+
+	// Read counter results
+	branch_miss = read_counter(0);
+	cycle_count = read_counter(1);
+	// Accumulate counter results
+	branch_miss_sum += branch_miss;
+	cycle_count_sum += cycle_count;
+	}
+
+	// Average run counts
+	branch_miss_sum = branch_miss_sum/num_runs;
+	cycle_count_sum = cycle_count_sum/num_runs;
+	// Output results
+	std::cout<<std::dec<<"Smart operation  branch misses: "<<branch_miss_sum<<"\tcycle count: "<<cycle_count_sum<<std::endl;
 
 	return 0;
 }
